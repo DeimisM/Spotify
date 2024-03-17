7b8da3fa1e6f7184b9efa30c5f0c2bb2154c389c
DeimisM <deimis.mardosas@outlook.com>
2024-03-17 18:39:23 +0200

    alarm sound

--- Assets/Scripts/Synth.cs
+++ Assets/Scripts/Synth.cs
@@ -18,7 +18,9 @@ public class Synth : MonoBehaviour
         var samples = new float[44100 * 3];
         for (int i = 0; i < samples.Length; i++)
         {
-            samples[i] = Mathf.Sin(i / 44100f * 6.28f * frequency);
+            samples[i] = Mathf.Sin(i / 44100f * Mathf.PI * 2 * 853);
+            samples[i] += Mathf.Sin(i / 44100f * Mathf.PI * 2 * 960);
+            samples[i] /= 2;
         }
 
         clip.SetData(samples, 0);
